d5d6e978dd3acefa0d9f07de93ed5310d32ad922
ugyanakkor <ifjutibor@gmail.com>
2019-12-22 19:41:31 +0100

    Header and Components

--- two-day-rule/src/app/app.module.ts
+++ two-day-rule/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HabitsComponent } from './habits/habits.component';
+import { HabitListComponent } from './habits/habit-list/habit-list.component';
+import { HabitDetailComponent } from './habits/habit-detail/habit-detail.component';
+import { HabitItemComponent } from './habits/habit-list/habit-item/habit-item.component';
+import { HabitEditComponent } from './habits/habit-list/habit-edit/habit-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HabitsComponent,
+    HabitListComponent,
+    HabitDetailComponent,
+    HabitItemComponent,
+    HabitEditComponent
   ],
   imports: [
     BrowserModule
---
+++ two-day-rule/src/app/habits/habits.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-habits',
+  templateUrl: './habits.component.html',
+  styleUrls: ['./habits.component.css']
+})
+export class HabitsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
