8bb939d8c6cb4ee32441a4dee1ffa33e5f09343f
ugyanakkor <ifjutibor@gmail.com>
2019-12-22 19:51:58 +0100

    Header and Components correctly

---
+++ two-day-rule/src/app/habits/habit-list/habit-edit/habit-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-habit-edit',
+  templateUrl: './habit-edit.component.html',
+  styleUrls: ['./habit-edit.component.css']
+})
+export class HabitEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ two-day-rule/src/app/habits/habit-list/habit-item/habit-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-habit-item',
+  templateUrl: './habit-item.component.html',
+  styleUrls: ['./habit-item.component.css']
+})
+export class HabitItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
